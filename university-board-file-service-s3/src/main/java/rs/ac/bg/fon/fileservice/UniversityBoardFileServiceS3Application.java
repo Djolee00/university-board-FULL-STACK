1dc53746b1397a7e3f9c0e788273db6b1c612e78
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-08-07 19:38:14 +0200

    FS: Initialization of file service

---
+++ university-board-file-service-s3/src/main/java/rs/ac/bg/fon/fileservice/UniversityBoardFileServiceS3Application.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityBoardFileServiceS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityBoardFileServiceS3Application.class, args);
+    }
+
+}
