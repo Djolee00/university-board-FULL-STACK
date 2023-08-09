e518e9fca211629fe83552243549ece36620ea25
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-08-09 19:34:19 +0200

    BE: Project initialization, gitignore and properties setup

---
+++ university-board-backend/src/main/java/rs/ac/fon/universityboardbackend/UniversityBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.fon.universityboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityBoardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityBoardBackendApplication.class, args);
+    }
+
+}
