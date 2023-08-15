4e180875cf4ba1a035dab4cd29c25ebad4b90f53
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-08-15 22:39:43 +0200

    BE: Password regex added

--- university-board-backend/src/main/java/rs/ac/fon/universityboardbackend/web/dto/patch/UserProfilePatchDto.java
+++ university-board-backend/src/main/java/rs/ac/fon/universityboardbackend/web/dto/patch/UserProfilePatchDto.java
@@ -1,7 +1,16 @@
 package rs.ac.fon.universityboardbackend.web.dto.patch;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import rs.ac.fon.universityboardbackend.web.dto.base.BaseDto;
 
-public record UserProfilePatchDto(@NotNull String oldPassword, @NotNull String newPassword)
+public record UserProfilePatchDto(
+        @NotNull String oldPassword,
+        @NotNull
+                @Pattern(
+                        regexp =
+                                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,}$",
+                        message =
+                                "Password must have at least one digit, one uppercase letter and one special character. Minimum length is 6 characters")
+                String newPassword)
         implements BaseDto {}
