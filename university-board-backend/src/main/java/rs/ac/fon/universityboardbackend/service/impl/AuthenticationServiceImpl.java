74379666c27674d666ba0cb33617842e69579085
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-09-01 18:29:05 +0200

    BE: Added privileges with auth response

--- university-board-backend/src/main/java/rs/ac/fon/universityboardbackend/service/impl/AuthenticationServiceImpl.java
+++ university-board-backend/src/main/java/rs/ac/fon/universityboardbackend/service/impl/AuthenticationServiceImpl.java
@@ -1,10 +1,13 @@
 package rs.ac.fon.universityboardbackend.service.impl;
 
 import java.time.ZoneOffset;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+import rs.ac.fon.universityboardbackend.model.user.Privilege;
+import rs.ac.fon.universityboardbackend.model.user.Privilege.PrivilegeCode;
 import rs.ac.fon.universityboardbackend.repository.UserProfileRepository;
 import rs.ac.fon.universityboardbackend.service.AuthenticationService;
 import rs.ac.fon.universityboardbackend.service.JwtService;
@@ -37,6 +40,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                                 .toInstant()
                                 .atOffset(ZoneOffset.ofHours(2)))
                 .employeeUuid(user.getEmployee().getUuid())
+                .privileges(
+                        Stream.concat(
+                                        user.getPrivileges().stream().map(Privilege::getCode),
+                                        user.getRole().getPrivileges().stream()
+                                                .map(Privilege::getCode))
+                                .toArray(PrivilegeCode[]::new))
                 .build();
     }
 }
--- university-board-backend/src/main/java/rs/ac/fon/universityboardbackend/web/dto/response/JwtAuthenticationResponse.java
+++ university-board-backend/src/main/java/rs/ac/fon/universityboardbackend/web/dto/response/JwtAuthenticationResponse.java
@@ -3,7 +3,11 @@ package rs.ac.fon.universityboardbackend.web.dto.response;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.Builder;
+import rs.ac.fon.universityboardbackend.model.user.Privilege.PrivilegeCode;
 
 @Builder
 public record JwtAuthenticationResponse(
-        String token, OffsetDateTime expirationTime, UUID employeeUuid) {}
+        String token,
+        OffsetDateTime expirationTime,
+        UUID employeeUuid,
+        PrivilegeCode[] privileges) {}
